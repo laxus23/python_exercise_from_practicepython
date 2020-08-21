63388b14eba2e19eb45898451cccf6edfb50e55e
laxus <laxus2323@o2.pl>
2020-08-21 11:18:04 +0200

    add solution for add or even exercise

---
+++ 2_odd_or_even.py
@@ -0,0 +1,29 @@
+def odd_or_even(user_number: int, num: int, check: int):
+    """
+    Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to
+    the user. Hint: how does an even / odd number react differently when divided by 2?
+
+    Extras:
+
+    1. If the number is a multiple of 4, print out a different message.
+    2. Ask the user for two numbers: one number to check (
+    call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not,
+    print a different appropriate message.
+    """
+    # user_number = int(input("Provide a number: "))
+    if user_number % 2 == 0:
+        if user_number % 4 == 0:
+            print(f'Your number is {user_number} and this is even number and it is a multiple of 4')
+        else:
+            print(f'Your number is {user_number} and this is even number')
+    else:
+        print(f'Your number is {user_number} and this is odd number')
+    # num = int(input("Provide a number to check: "))
+    # check = int(input("Provide a divide number: "))
+    if num % check == 0:
+        print(f'{num}\n{check}\nCheck divides evenly into num')
+    else:
+        print(f'{num}\n{check}\nCheck not divides evenly into num')
+
+
+odd_or_even(user_number=10, num=20, check=5)
